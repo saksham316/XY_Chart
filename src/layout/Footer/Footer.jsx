d23ab1d0f558357d2c39e762cdbb0bd9b963ede7
saksham <phantomdx316@gmail.com>
2024-04-28 09:11:35 +0530

    footer position changes

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
 
 
-        <footer class="bg-white  shadow dark:bg-gray-900 absolute">
+        <footer class="bg-white  shadow dark:bg-gray-900 bottom-0">
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
