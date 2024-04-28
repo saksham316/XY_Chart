de04b00851ae87499ff03dc282eb07040b7f5f89
saksham <phantomdx316@gmail.com>
2024-04-28 13:46:02 +0530

    ht changes

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <nav
-      className="flex-no-wrap  flex w-full items-center justify-center bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap  lg:py-4 font-bold text-3xl text-white">
+      className="h-[10vh] flex-no-wrap  flex w-full items-center justify-center bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap  lg:py-4 font-bold text-3xl text-white">
       Chart
     </nav>
   )
